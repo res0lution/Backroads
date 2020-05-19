cdb6af7da7d958aedaecf0e6c6bf290f84f98728
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-19 13:45:01 +0300

    Created Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-import { FaAdobe } from "react-icons/fa"
 
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 
 export default () => (
   <Layout>
-    <div>hello from backroads</div>
+    <SimpleHero>
+      <Banner
+        title="Continue exploring"
+        info="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, assumenda."
+      >
+        <Link to="/tours" className="btn-white">Explore tours</Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
