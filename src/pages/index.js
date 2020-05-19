f0dbadc53dfe70dc7332d7e5353f022d7b17c103
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-19 14:43:45 +0300

    Created Services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
 export default () => (
   <Layout>
@@ -13,10 +14,13 @@ export default () => (
         title="Continue exploring"
         info="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, assumenda."
       >
-        <Link to="/tours" className="btn-white">Explore tours</Link>
+        <Link to="/tours" className="btn-white">
+          Explore tours
+        </Link>
       </Banner>
     </SimpleHero>
 
-    <About/>
+    <About />
+    <Services />
   </Layout>
 )
