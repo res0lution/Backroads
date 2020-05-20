4f2596c7fa0302ec2b0b6997a38e6cf823ebd1a4
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-20 19:14:13 +0300

    Blog templates setup

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { graphql } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+import styles from "../css/blog.module.css"
+import BlogCard from "../components/Blog/BlogCard"
+import Title from "../components/Title"
+import Layout from "../components/Layout"
+
+const Blog = () => {
+  return <div>this is blogs template</div>
+}
+
+export default Blog
---
+++ src/templates/blog-template.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { graphql } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import Layout from "../components/Layout"
+import styles from "../css/single-blog.module.css"
+
+const Blog = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json },
+  } = data.post
+
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div className="rich">
+            <h3>this is awesome image</h3>
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+            <p>images provided by john doe</p>
+          </div>
+        )
+      },
+    },
+  }
+
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <div className={styles.center}>
+          <h1>{title}</h1>
+          <h4>published at : {published}</h4>
+
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
+          
+          <AniLink fade to="/blog" className="btn-primary">
+            all posts
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getPost($slug: String!) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do, YYYY")
+      text {
+        json
+      }
+    }
+  }
+`
+
+export default Blog
